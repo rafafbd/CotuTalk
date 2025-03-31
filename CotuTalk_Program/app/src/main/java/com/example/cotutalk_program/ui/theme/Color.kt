443f35fee70bc461d65b89ce79faf2d20ce88814
Rafael Fazion B. Dias <rafafazion9@gmail.com>
2025-03-31 09:01:45 -0300

    responder

--- CotuTalk_Program/app/src/main/java/com/example/cotutalk_program/ui/theme/Color.kt
+++ CotuTalk_Program/app/src/main/java/com/example/cotutalk_program/ui/theme/Color.kt
@@ -13,6 +13,7 @@ val branco = Color(0xFFFFFFFF)
 val roxo80 = Color(0xFF130519)
 val roxo70 = Color(0xFF38104C)
 val roxo60 = Color(0xFF54146D)
+val roxo50 = Color(0xFF340C48)
 
 val lilas20 = Color(0xFF64549C)
 val lilas10 = Color(0xFFA03CD4)
