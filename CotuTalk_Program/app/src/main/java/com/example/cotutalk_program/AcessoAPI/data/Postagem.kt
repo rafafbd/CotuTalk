d6169f842212c3575137c1be10a19c71d40122bd
Rafael Fazion B. Dias <rafafazion9@gmail.com>
2025-05-12 09:48:10 -0300

    Integração api início
    Implementação básica do Retrofit e projeções de classes e interfaces.

---
+++ CotuTalk_Program/app/src/main/java/com/example/cotutalk_program/AcessoAPI/data/Postagem.kt
@@ -0,0 +1,5 @@
+package com.example.cotutalk_program.AcessoAPI.data
+
+data class Postagem (
+    val id: Int
+)
---
+++ CotuTalk_Program/app/src/main/java/com/example/cotutalk_program/AcessoAPI/network/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package com.example.clienteapp.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://seu ip" // Use o endereço da sua API
+
+    object RetrofitClient {
+        private const val BASE_URL = "http://seu-ip:porta" // substitua pelo IP real
+
+        private val retrofit: Retrofit by lazy {
+            Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+        }
+
+        val usuarioInstance: UsuarioApi by lazy {
+            retrofit.create(UsuarioApi::class.java)
+        }
+
+        val postagemInstance: PostagemApi by lazy {
+            retrofit.create(PostagemApi::class.java)
+        }
+    }
+
+}
