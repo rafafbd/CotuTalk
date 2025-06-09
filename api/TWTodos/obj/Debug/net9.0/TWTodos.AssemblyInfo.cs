bfa1718c26a301022df872189830ec138b404d04
Vinicius Pinheiro <vinicius.gdpinheiro@gmail.com>
2025-06-09 08:58:16 -0300

    .

--- api/TWTodos/obj/Debug/net9.0/TWTodos.AssemblyInfo.cs
+++ api/TWTodos/obj/Debug/net9.0/TWTodos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TWTodos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a35e04a4e3707c119f994d18b19f50deeb994299")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+832f5155033b0c0e9a55f2b51a0d24f01c770563")]
 [assembly: System.Reflection.AssemblyProductAttribute("TWTodos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TWTodos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
